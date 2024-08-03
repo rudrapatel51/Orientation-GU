4963812a8cf436fe9e886e4cd3d9b98c3948f849
rudrapatel51 <rudrapokar5105@gmail.com>
2024-08-03 09:53:24 +0530

    rudra college api

--- src/Components/QRCodeScanner.jsx
+++ src/Components/QRCodeScanner.jsx
@@ -53,7 +53,7 @@ const QRCodeScanner = () => {
                 const qrData = JSON.parse(data);
 
                 if (qrData.name && qrData.mobile && qrData.email) {
-                    const response = await axios.get('https://sheetdb.io/api/v1/4aa2f9sloj95x');
+                    const response = await axios.get('https://sheetdb.io/api/v1/hcd2jiaofgal7');
                     const sheetData = response.data;
 
                     const existingEntry = sheetData.find(entry => entry.mobile === qrData.mobile);
@@ -65,7 +65,7 @@ const QRCodeScanner = () => {
                             window.location.reload();
                         }, 1000);
                     } else {
-                        await axios.post('https://sheetdb.io/api/v1/4aa2f9sloj95x', {
+                        await axios.post('https://sheetdb.io/api/v1/hcd2jiaofgal7', {
                             data: {
                                 name: qrData.name,
                                 mobile: qrData.mobile,
